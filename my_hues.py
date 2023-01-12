2609b18e31259ee641eec2a598b75e0f65712d15
Leon Hatton <elementalsystems1@gmail.com>
2023-01-11 20:57:03 -0500

    Update my_hues.py
    Added feature to automate background color changes and fades by syncing them to the iterable count. Variable created for this is t.bg_count.

--- my_hues.py
+++ my_hues.py
@@ -1,14 +1,102 @@
-# my_hues.py contains color manipulation scripts associated with master_mandala_maker.py
-                 # by LeonRHatton
-
+'''my_hues.py contains color manipulation scripts associated with master_mandala_maker.py
+                  by LeonRHatton
+'''
 import My_template as t
 import random
 import turtle
 turtle.colormode(255)
 
 
+
+
+#  Gradually change screen background hues between  two hues
+def bg_fade_dark_to_yellow():
+    if t.bg_count >= 255:
+                turtle.bgcolor(255, 255, 0) # Stops at Yellow Hue
+    else:
+       
+        turtle.bgcolor(t.bg_count, t.bg_count, 0) # Counter t.iterable starts at zero; background starts black, fades to yellow
+
+def bg_fade_yellow_to_dark():
+    if t.bg_count >= 254:
+                turtle.bgcolor(0, 0, 10) # Stops at black hue
+    else:
+        turtle.bgcolor(255 - t.bg_count, 255 - t.bg_count, 10) # Counter t.iterable starts at zero; background starts yellow, fades to black
+'''************************************************************************************************************************************'''
+'''************************************************************************************************************************************'''
+
+
+
+
+def bg_fade_dark_to_skyblue():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 255, 255) # Stops at sky blue hue
+    else:
+        turtle.bgcolor(0, t.bg_count, t.bg_count) # Counter t.iterable starts at zero; background starts black, fades to sky blue
+
+def bg_fade_skyblue_to_dark():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 0, 0) # Stops at black hue
+    else:
+        turtle.bgcolor(0, 255 - t.bg_count, 255 - t.bg_count) # Counter t.iterable starts at zero; background starts sky blue, fades to black
+'''************************************************************************************************************************************'''
+
+def bg_fade_dark_to_green():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 255, 0) # Stops at green hue
+    else:
+        turtle.bgcolor(0, t.bg_count, 0) # Counter t.iterable starts at zero; background starts black, fades to green
+
+def bg_fade_green_to_dark():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 0, 0) # Stops at black hue
+    else:
+        turtle.bgcolor(0, 255 - t.bg_count, 0) # Counter t.iterable starts at zero; background starts green, fades to black
+'''************************************************************************************************************************************'''
+
+
+
+
+def bg_fade_dark_to_green():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 255, 0) # Stops at green hue
+    else:
+        turtle.bgcolor(0, t.bg_count, 0) # Counter t.iterable starts at zero; background starts black, fades to green
+
+def bg_fade_green_to_dark():
+    if t.bg_count >= 255:
+        turtle.bgcolor(0, 0, 0) # Stops at black hue
+    else:
+        turtle.bgcolor(0, 255 - t.bg_count, 0) # Counter t.iterable starts at zero; background starts green, fades to black
+'''************************************************************************************************************************************'''
+
+
+
+
+def bg_fade_dark_to_green_to_dark():
+    if t.bg_count <= 255:
+        turtle.bgcolor(0, t.bg_count, 0) # Stops at green hue
+    else:
+       turtle.bgcolor(0, 255 - t.bg_count % 255, 0) # Counter t.iterable starts at zero; background starts black, fades to green
+
+'''************************************************************************************************************************************'''
+#Work on 
+# def cycle_skyblue_to_dark():
+#     new_count = t.iterable - t.bg_count
+#     if t.iterable in range(0, 255):
+#          turtle.bgcolor(0, 255 - t.bg_count, 255 - t.bg_count) # Counter t.iterable starts at zero; background starts sky blue, fades to black
+#     elif t.iterable in range(201,400):
+#          turtle.bgcolor(0, 0,  t.bg_count % 255) # Counter t.iterable starts at zero; background starts black, fades to sky blue
+#     else:
+#          turtle.bgcolor(0, 0, 0) # Counter t.iterable starts at zero and stays at dark setting   
+    
+
+
+
+
+
 #random red hues
-def pick_red():
+def pick_red(): #t.lu
     r = random.randint(190, 255)
     g = random.randint(0, 25)
     b = random.randint(50, 85)
@@ -16,6 +104,7 @@ def pick_red():
     G = g
     B = b
     t.lu.pencolor(R, G, B)
+    return t.lu
 #     print('The pick_red pen is t.lu')
 
 
@@ -28,6 +117,7 @@ def pick_blue():
     M = g
     N = b
     t.lb.pencolor(L, M, N)
+    return t.lb
 #     print('The pick_blue pen is t.lb')
 
 
@@ -41,6 +131,7 @@ def pick_green():
     P = g
     Q = b
     t.lg.pencolor(O, P, Q)
+    return t.lg
 #     print('The pick_green pen is t.lg')
 
 
@@ -54,6 +145,7 @@ def pick_indigo():
     T = g
     U = b
     t.li.pencolor(S, T, U)
+    return t.li
 #     print('The pick_indigo pen is t.li')
 
 #random magenta hues
@@ -65,6 +157,7 @@ def pick_magenta():
     J = g
     K = b
     t.me.pencolor(I, J, K)
+    return t.me
 #     print('The pick_magenta pen is t.me')
 
 #random gold hues
@@ -75,7 +168,8 @@ def pick_gold():
     V = r
     W = g
     X = b
-    return(t.la.pencolor(V, W, X))
+    t.la.pencolor(V, W, X)
+    return t.la
 #     )
 #     print('The pick_gold pen is t.la')
 
@@ -88,6 +182,7 @@ def pick_dark():
     Y = g
     Z = b
     t.lz.pencolor(X, Y, Z)
+    return t.lz
 #     print('The pick_dark pen is t.lz')
 
 
@@ -101,6 +196,7 @@ def pick_light():
     B = g
     C = b
     t.le.pencolor(A, B, C)
+    return t.le
 #     print('The pick_light pen is t.le')
 
 
@@ -112,7 +208,8 @@ def pick_random():
     D = r
     E = g
     F = b
-    t.me.pencolor(D, E, F)
+    t.ce.pencolor(D, E, F)
+    return t.ce
 #     print('The pick_random pen is t.me')
 
 def pick_random_a():
@@ -123,6 +220,7 @@ def pick_random_a():
     E = g
     F = b
     t.lr.pencolor(D, E, F)
+    return t.lr
 #     print('The pick_random_a pen is t.lr')
     
     
@@ -135,7 +233,8 @@ def pick_dot():
     Y = g
     Z = b
     t.ld.pencolor(X, Y, Z)
+    return t.ld
 #     print('The pick_dot pen is t.ld')
     
-dict = {'pick_dot': 't.ld', 'pick_random_a': 't.lr', 'pick_random': 't.me', 'pick_light': 't.le', 'pick_dark': 't.lz', 'pick_gold': 't.la', 'pick_magenta': 't.me', 'pick_indigo': 't.li', 'pick_green': 't.lg', 'pick_blue': 't.lb', 'pick_red': 't.lu'}
-# print ("dict ['pick_gold'] :   ", dict ['pick_gold']  )
+hue_dict = {'pick_dot': 't.ld', 'pick_random_a': 't.lr', 'pick_random': 't.ce', 'pick_light': 't.le', 'pick_dark': 't.lz', 'pick_gold': 't.la', 'pick_magenta': 't.me', 'pick_indigo': 't.li', 'pick_green': 't.lg', 'pick_blue': 't.lb', 'pick_red': 't.lu'}
+# print( str(hue_dict))
